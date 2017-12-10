5b4173f710079c5170cb7b8e24c59b57e37e70d7
user <user@DESKTOP-C3T5D5F>
2017-12-10 13:41:07 +0800

    201712101341

---
+++ MyJava/src/tw/org/iii/myjava/Brad01.java
@@ -0,0 +1,12 @@
+package tw.org.iii.myjava;
+
+public class Brad01 {
+	public static void main(String[] args) {
+		// Data Type
+		// Integer: byte(2^8) short(2^16) int(2^32) long(2^64)
+		byte var1;	// -128 ~ 127
+		var1 = 123;
+		System.out.println(var1);
+		
+	}
+}
