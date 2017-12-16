652736dd0ac7f34576bea67b942d9810d2a63ce9
user <user@DESKTOP-C3T5D5F>
2017-12-16 14:16:26 +0800

    Brad07891099

---
+++ MyJava/src/tw/org/iii/myjava/Brad08.java
@@ -0,0 +1,18 @@
+package tw.org.iii.myjava;
+
+public class Brad08 {
+
+	public static void main(String[] args) {
+		int i = 0;
+		for (m1(); i<10;System.out.println("---")) {
+			System.out.println(i++);
+		}
+	}
+	
+	static void m1() {
+		System.out.println("First");
+		System.out.println("Brad");
+	}
+	
+
+}
---
+++ MyJava/src/tw/org/iii/myjava/Brad09.java
@@ -0,0 +1,15 @@
+package tw.org.iii.myjava;
+
+public class Brad09 {
+
+	public static void main(String[] args) {
+		int[] ary1;
+		int ary2[];
+		boolean[] ary3 = new boolean[3];	// 1. 元素個數是固定的; 2. 型別也是固定
+		System.out.println(ary3[0]);
+		System.out.println(ary3[1]);
+		System.out.println(ary3[2]);
+		//System.out.println(ary3[3]);
+	}
+
+}
