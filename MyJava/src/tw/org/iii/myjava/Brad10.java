a926a76c9eb150b3426ae5d616255b171388324e
user <user@DESKTOP-C3T5D5F>
2017-12-16 15:03:22 +0800

    Brad11

--- MyJava/src/tw/org/iii/myjava/Brad10.java
+++ MyJava/src/tw/org/iii/myjava/Brad10.java
@@ -5,14 +5,19 @@ public class Brad10 {
 	public static void main(String[] args) {
 		int[] p = new int[6];	// [0]:0; [1]:0; ....[5]:0
 		
-		for (int i=0; i<100; i++) {
-			int rand = (int)(Math.random()*6);
-			p[rand]++;
+		for (int i=0; i<1000000; i++) {
+			int rand = (int)(Math.random()*9);	// 0 , ... 8
+			p[rand<=5?rand:rand-3]++;
 		}
 		
 		for (int i=0; i<p.length; i++) {
 			System.out.println((i+1) + "點出現" + p[i] + "次");
 		}
+		
+		for (int v : p) {	// for-each
+			System.out.println(v);
+		}
+		
 		
 	}
 
---
+++ MyJava/src/tw/org/iii/myjava/Brad11.java
@@ -0,0 +1,19 @@
+package tw.org.iii.myjava;
+
+public class Brad11 {
+
+	public static void main(String[] args) {
+		int[][] a = new int[3][4];
+		int[][] b = new int[3][];
+		
+		b[0] = new int[2];
+		b[1] = new int[3];
+		b[2] = new int[4];
+		
+		System.out.println(b[1].length);
+		
+		
+		
+	}
+
+}
