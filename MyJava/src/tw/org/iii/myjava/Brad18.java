78038ef5b723645f964ae93443397045d5ef0c17
user <user@DESKTOP-C3T5D5F>
2017-12-23 09:48:12 +0800

    201712230948

--- MyJava/src/tw/org/iii/myjava/Brad18.java
+++ MyJava/src/tw/org/iii/myjava/Brad18.java
@@ -4,9 +4,10 @@ public class Brad18 {
 
 	public static void main(String[] args) {
 		Scooter s1 = new Scooter(2);
-		s1.upSpeed();
-		s1.downSpeed();
-		System.out.println(s1.getSpeed());
+		Scooter s2 = new Scooter(2.0f);
+		ScooterV2 s3 = new ScooterV2();
+		System.out.println("-----");
+		ScooterV2 s4 = new ScooterV2(1);
 	}
 
 }
--- MyJava/src/tw/org/iii/myjava/Scooter.java
+++ MyJava/src/tw/org/iii/myjava/Scooter.java
@@ -5,4 +5,22 @@ public class Scooter extends Bike{
 		// super();	==> 呼叫父類別的建構式 => 建立一個父類別的物件實體
 		System.out.println("Scooter(int)");
 	}
+	public Scooter(double a) {
+		System.out.println("Scooter(double)");
+	}
+}
+class ScooterV2 extends Scooter {
+	ScooterV2(){
+		// super(); => where?
+		super(3.0);
+		System.out.println("ScooterV2()");
+	}
+	
+	ScooterV2(int a){
+		this();
+		System.out.println("ScooterV2(int)");
+	}
+	
+	
+	
 }
