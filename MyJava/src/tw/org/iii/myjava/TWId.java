7a4e325863e54a7c29037522530f6af7b2d7a015
user <user@DESKTOP-C3T5D5F>
2017-12-23 10:34:17 +0800

    201712231034

--- MyJava/src/tw/org/iii/myjava/TWId.java
+++ MyJava/src/tw/org/iii/myjava/TWId.java
@@ -1,5 +1,37 @@
 package tw.org.iii.myjava;
 
 public class TWId {
-
+	private String id;
+	
+	public TWId() {
+		this((int)(Math.random()*2)==0?true:false,
+				(int)(Math.random()*26));
+	}
+	public TWId(boolean isMale) {
+		this(isMale, (int)(Math.random()*26));
+	}
+	public TWId(int area) {
+		this((int)(Math.random()*2)==0?true:false , area);
+	}
+	public TWId(boolean isMale, int area) {
+		
+		
+	}
+	
+//	public TWId(String id) {
+//		this.id = id;
+//	}
+	
+	public String getId() {
+		return id;
+	}
+	
+	public boolean isMale() {
+		return true;
+	}
+	
+	public String getArea() {
+		return "台中市";
+	}
+	
 }
