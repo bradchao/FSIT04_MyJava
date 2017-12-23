cad8a6675695d112b0f5349bfb0b8c2f7d7bd6d3
user <user@DESKTOP-C3T5D5F>
2017-12-23 14:26:41 +0800

    201712231426

--- MyJava/src/tw/org/iii/myjava/Brad21.java
+++ MyJava/src/tw/org/iii/myjava/Brad21.java
@@ -2,9 +2,11 @@ package tw.org.iii.myjava;
 
 public class Brad21 {
 	public static void main(String[] args) {
-		System.out.println(Brad211.b);
+//		System.out.println(Brad211.b);
 //		Brad211 obj1 = new Brad211();
 //		Brad211 obj2 = new Brad211();
+		
+		Brad212 obj3 = new Brad212();
 	}
 }
 class Brad211 {
@@ -20,4 +22,8 @@ class Brad211 {
 		System.out.println("Brad211()");
 	}
 	void m1() {System.out.println("Brad211:m1()");}
+}
+class Brad212 extends Brad211 {
+	static {System.out.println("Brad212:static{}");}
+	Brad212(){System.out.println("Brad212()");}
 }
