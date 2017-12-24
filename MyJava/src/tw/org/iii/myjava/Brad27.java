d2f9160b16df3eed61de02ec0fe9afd7a56e3ad2
user <user@DESKTOP-C3T5D5F>
2017-12-24 09:59:11 +0800

    201712240959

---
+++ MyJava/src/tw/org/iii/myjava/Brad27.java
@@ -0,0 +1,27 @@
+package tw.org.iii.myjava;
+
+public class Brad27 {
+	public static void main(String[] args) {
+
+	}
+}
+interface Shape {
+	double calLength();
+	double calArea();
+}
+class Circle implements Shape {
+	public double calLength() {
+		return 0;
+	}
+	public double calArea() {
+		return 0;
+	}
+}
+class Tri implements Shape {
+	public double calLength() {
+		return 0;
+	}
+	public double calArea() {
+		return 0;
+	}
+}
