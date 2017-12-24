9359ca73a2f0bfb7c0677f033dbe7f831bf5dd92
user <user@DESKTOP-C3T5D5F>
2017-12-24 14:15:18 +0800

    201712241415

--- MyJava/src/tw/org/iii/myjava/Brad31.java
+++ MyJava/src/tw/org/iii/myjava/Brad31.java
@@ -3,22 +3,17 @@ package tw.org.iii.myjava;
 public class Brad31 {
 
 	public static void main(String[] args) {
-		int a = 10, b = 3;
-		System.out.println("1---");
 		try {
+			int a = 10, b = 0;
 			int c = a / b;
-		}catch (ArithmeticException ae) {
-			System.out.println("E1");
-		}
-		System.out.println("2---");
-		int[] d = {1,2,3,4};
-		System.out.println("3---");
-		try {
-			System.out.println(d[3]);
+			int[] d = {1,2,3,4};
+			System.out.println(d[4]);
 		}catch(ArrayIndexOutOfBoundsException ee) {
+			System.out.println("E1");
+		}catch(ArithmeticException e2) {
 			System.out.println("E2");
 		}
-		System.out.println("4---");
+		System.out.println("End");
 	}
 
 }
