00c3657556f032e81cdef7d3997c36663cee15df
user <user@DESKTOP-C3T5D5F>
2017-12-24 11:22:53 +0800

    201712241122

--- MyJava/src/tw/org/iii/myjava/Brad24.java
+++ MyJava/src/tw/org/iii/myjava/Brad24.java
@@ -1,11 +1,16 @@
 package tw.org.iii.myjava;
 
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
-public class Brad24 extends JFrame{
+public class Brad24 extends JFrame implements ActionListener{
 	private JTextField input;
 	private JButton guess;
 	private JTextArea log;
@@ -17,8 +22,16 @@ public class Brad24 extends JFrame{
 		guess = new JButton("猜");
 		log = new JTextArea();
 		
+		setLayout(new BorderLayout());
 		
+		JPanel top = new JPanel(new BorderLayout());
+		top.add(guess, BorderLayout.EAST);
+		top.add(input, BorderLayout.CENTER);
 		
+		add(top, BorderLayout.NORTH);
+		add(log, BorderLayout.CENTER);
+		
+		guess.addActionListener(this);
 		
 		setVisible(true);
 		setSize(640, 480);
@@ -27,6 +40,11 @@ public class Brad24 extends JFrame{
 	
 	public static void main(String[] args) {
 		new Brad24();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		System.out.println("got it");
 	}
 
 }
