a451b956354d3a8563fd17fe699238873fe5ce11
user <user@DESKTOP-C3T5D5F>
2018-01-07 15:47:25 +0800

    201801071547

--- MyJava/src/MyDrawer.java
+++ MyJava/src/MyDrawer.java
@@ -2,15 +2,58 @@ import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.HashMap;
+import java.util.LinkedList;
 
 import javax.swing.JPanel;
 
 public class MyDrawer extends JPanel {
+	private LinkedList<LinkedList<HashMap<String, Integer>>> lines;
 	
 	public MyDrawer() {
 		setBackground(Color.GREEN);
+		addMouseMotionListener(new MouseAdapter() {
+			@Override
+			public void mouseDragged(MouseEvent event) {
+				super.mouseDragged(event);
+				//System.out.println("drag");
+				doMouseEvent(event);
+			}
+		});
+		
+		addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent event) {
+				super.mousePressed(event);
+				//System.out.println("press");
+				doNewLine(event);
+			}
+		});
+		
+		lines = new LinkedList<>();
+		
+	}
+	
+	private void doNewLine(MouseEvent event) {
+		LinkedList<HashMap<String, Integer>> line = new LinkedList<>();
+		HashMap<String,Integer> dot = new HashMap<>();
+		dot.put("x", event.getX());
+		dot.put("y", event.getY());
+		line.add(dot);
+		lines.add(line);
+	}
+	
+	private void doMouseEvent(MouseEvent event) {
+		HashMap<String,Integer> dot = new HashMap<>();
+		dot.put("x", event.getX());
+		dot.put("y", event.getY());
+		lines.getLast().add(dot);
+		repaint();
 	}
 
+	
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
@@ -19,7 +62,13 @@ public class MyDrawer extends JPanel {
 		
 		g2d.setColor(Color.BLUE);
 		g2d.setStroke(new BasicStroke(4));
-		g2d.drawLine(0, 0, 200, 300);
+		for (LinkedList<HashMap<String, Integer>> line : lines) {
+			for (int i=1; i<line.size(); i++) {
+				HashMap<String,Integer> p0 = line.get(i-1);
+				HashMap<String,Integer> p1 = line.get(i);
+				g2d.drawLine(p0.get("x"), p0.get("y"), p1.get("x"), p1.get("y"));
+			}
+		}
 		
 	}
 	
